69232760dc2f359501e777b4bb2fdeae93f8ac3a
HollowDS <denis.osipov.2017@inbox.ru>
2024-05-07 15:21:20 +0300

    Настройка роутинга

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
 import Button from "./components/Button/Button";
+import Input from "./components/Input/Input";
 
 function App() {
-  return <Button onClick={() => console.log(2)}>Кнопка</Button>;
+  return (
+    <>
+      <Button onClick={() => console.log(2)}>Обычная кнопка</Button>
+      <Button appearance="big" onClick={() => console.log(2)}>
+        Большая кнопка
+      </Button>
+      <Input placeholder="email" />
+    </>
+  );
 }
 
 export default App;
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,9 +3,20 @@ import styles from "./Button.module.css";
 import { ButtonProps } from "./Button.props";
 import cn from "classnames";
 
-export function Button({ children, className, ...props }: ButtonProps) {
+export function Button({
+  children,
+  className,
+  appearance = "small",
+  ...props
+}: ButtonProps) {
   return (
-    <button className={cn(styles.button, styles.accent, className)} {...props}>
+    <button
+      className={cn(styles.button, styles.accent, className, {
+        [styles.small]: appearance === "small",
+        [styles.big]: appearance === "big",
+      })}
+      {...props}
+    >
       {children}
     </button>
   );
